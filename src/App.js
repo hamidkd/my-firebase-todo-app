62e6c880a4a9a859d19d1d2b679cbc9b021d5754
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-05-04 12:24:24 -0400

    App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "React";
+
+const App = () => {
+  return <div>Hello World !!!</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
 const rootDiv = document.getElementById("root");
 
-ReactDOM.render(<div>Hello World</div>,rootDiv);
-    
+ReactDOM.render(<App/>,rootDiv);
